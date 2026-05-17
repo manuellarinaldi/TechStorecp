5ee4b075d6e96454b1b8e27c8ca76d6ae636cdc1
manuellarinaldi <manuellarinaldi@gmail.com>
2026-05-17 16:54:47 -0300

    estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { AppRoutes } from './routes/AppRoutes';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-shell">
+        <Header />
+        <main className="app-main">
+          <div className="app-container">
+            <AppRoutes />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
